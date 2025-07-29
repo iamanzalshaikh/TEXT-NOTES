e6fd3893050a6839dc6cbd10540a5526e172ed33
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-07-29 15:35:51 +0530

    Update authController.js

--- TEXT-NOTES/backend/controllers/authController.js
+++ TEXT-NOTES/backend/controllers/authController.js
@@ -42,13 +42,15 @@ export const login = async (req, res) => {
         const token = generateToken(user._id);
 
 
+const isProduction = process.env.NODE_ENV === 'production';
 
-    res.cookie('token', token, {
-    httpOnly: true,
-    secure: true,            
-    sameSite: 'None',        
-    maxAge: 7 * 24 * 60 * 60 * 1000
+res.cookie('token', token, {
+  httpOnly: true,
+  secure: isProduction,
+  sameSite: isProduction ? 'None' : 'Lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 });
+
 
         res.status(200).json({ message: "Login successful", user });
     } catch (err) {
